1247e43effd5a4ecd8773314fa34f926b75114a2
Vincent Touzet <vincent.touzet@dotsafe.fr>
2013-04-29 18:12:55 +0200

    Change type of value from string to text

--- Entity/ConfigValue.php
+++ Entity/ConfigValue.php
@@ -67,7 +67,7 @@ class ConfigValue
     /**
      * @var string
      *
-     * @ORM\Column(name="value", type="string", length=255, nullable=true)
+     * @ORM\Column(name="value", type="text", nullable=true)
      */
     private $value = null;
 
